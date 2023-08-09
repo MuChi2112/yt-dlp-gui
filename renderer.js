a006c6dd7b43b4f8d9808383fda4664119e3c69d
Jack Lee <jack20499@gmail.com>
2023-08-09 16:42:49 +0800

    add start and end time

--- renderer.js
+++ renderer.js
@@ -3,12 +3,32 @@ const {
     ipcRenderer
 } = require('electron')
 
+var videoStartTime = "";
+var videoEndTime = "";
+
+document.getElementById('videoStartTime').addEventListener('input', () => {
+    videoStartTime = document.getElementById('videoStartTime').value;
+})
+
+document.getElementById('videoEndTime').addEventListener('input', () => {
+    videoEndTime = document.getElementById('videoEndTime').value;
+})
+
 document.getElementById('startBtn').addEventListener('click', () => {
+
+    let option = ""
+    if (videoStartTime.length != 0 || videoEndTime.length != 0) {
+        let start = videoStartTime; // 假設 videoStartTime 是你想要的開始時間
+        let end = videoEndTime; // 假設 videoEndTime 是你想要的結束時間
+        option += `--downloader ffmpeg --downloader-args "ffmpeg_i:-ss ${start} -to ${end}"`;
+    }
+
     const URL = document.getElementById('URL');
     console.log(URL.value)
 
-    ipcRenderer.send('download-video', URL.value)
+    ipcRenderer.send('download-video', option + URL.value)
 })
+
 ipcRenderer.on('yt-dlp-output', (event, output) => {
 
     document.getElementById('output-text').textContent += output;
